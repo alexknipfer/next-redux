31316c14e3015f8e37908788e4af475dfcf855e2
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 15:44:17 -0600

    restructure component layout

--- components/ListView/ListView.js
+++ components/ListView/ListView.js
@@ -1,26 +1,34 @@
 import AddForm from '../AddForm/AddForm'
 import { Grid } from 'semantic-ui-react'
+import TodoItem from '../TodoItem/TodoItem'
 
-export default () => (
-    <div className='list-view'>
-        <Grid padded>
-            <Grid.Row>
-                <Grid.Column>
-                    <h2>List View</h2>
-                </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-                <Grid.Column>
-                    <AddForm />
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-        <style jsx>{`
-            .list-view {
-                margin-top: 80px;
-                display: flex;
-                justify-content: center;
-            }
-        `}</style>
-    </div>
-)
+export default ({todoList, addTodo}) => {
+    return (
+        <div className='list-view'>
+            <Grid padded>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h2>List View</h2>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <AddForm todoList={todoList} addTodo={addTodo} />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <TodoItem todoList={todoList} />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            <style jsx>{`
+                .list-view {
+                    margin-top: 80px;
+                    display: flex;
+                    justify-content: center;
+                }
+            `}</style>
+        </div>
+    )
+}
