2b23c39c16abd1b7e58e369af7b78c5b2dc74a10
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 12:55:37 -0600

    import todo item container

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import LayoutContainer from '../containers/LayoutContainer'
 import ListView from '../components/ListView/ListView'
 import { Provider } from 'react-redux'
+import TodoItemContainer from '../containers/TodoItemContainer'
 import store from '../redux/store'
 
 export default () => (
     <Provider store={store}>
         <LayoutContainer>
             <ListView />
+            <TodoItemContainer />
         </LayoutContainer>
     </Provider>
 )
