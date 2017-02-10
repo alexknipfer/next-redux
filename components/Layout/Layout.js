6284ac428151dd7b92b104f3044678165f3a9a15
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 10:41:57 -0600

    pass actions to navigation

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
 import Navigation from '../Navigation/Navigation'
 
-export default ({ children }) => (
+export default ({ children, open, openDimmer, closeDimmer }) => (
     <div>
         <Head>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
         </Head>
-        <Navigation />
+        <Navigation open={open} openDimmer={openDimmer} closeDimmer={closeDimmer}/>
         {children}
     </div>
 )
