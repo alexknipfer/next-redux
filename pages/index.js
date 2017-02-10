5bf4bae6eed0d2978c020c2723d00f586141a05e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 15:45:20 -0600

    remove listview main render

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import LayoutContainer from '../containers/LayoutContainer'
-import ListView from '../components/ListView/ListView'
 import { Provider } from 'react-redux'
 import TodoItemContainer from '../containers/TodoItemContainer'
 import store from '../redux/store'
@@ -7,7 +6,6 @@ import store from '../redux/store'
 export default () => (
     <Provider store={store}>
         <LayoutContainer>
-            <ListView />
             <TodoItemContainer />
         </LayoutContainer>
     </Provider>
