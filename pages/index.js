c389908e78a100ca68658edc86c3d9b2998e79ea
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 10:59:40 -0600

    wrap the list view

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import LayoutContainer from '../containers/LayoutContainer'
+import ListView from '../components/ListView/ListView'
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 
 export default () => (
     <Provider store={store}>
-        <LayoutContainer />
+        <LayoutContainer>
+            <ListView />
+        </LayoutContainer>
     </Provider>
 )
