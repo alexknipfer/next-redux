1b0bca0446c97dd6e949b521b2f4488e6ec8d7a8
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-09 23:32:25 -0600

    navigation mock

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import { Menu } from 'semantic-ui-react'
+
+export default () => (
+    <Menu pointing fixed='top'>
+        <Menu.Item link>
+            Home
+        </Menu.Item>
+        <Menu.Item link>
+            Panel
+        </Menu.Item>
+    </Menu>
+)
