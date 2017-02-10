2d2e06fd8be8f79bc727aace60639a3b10030e7e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 15:43:36 -0600

    add item name and description

--- components/AddForm/AddForm.js
+++ components/AddForm/AddForm.js
@@ -1,13 +1,28 @@
 import { Button, Form } from 'semantic-ui-react'
 
-export default () => (
-    <Form>
-        <Form.Field>
-            <input type='text' placeholder='Item to add' name="item" id='itemName' />
-        </Form.Field>
-        <Form.Field>
-            <input type='text' placeholder='Item description' name="itemDescription" id='itemDescription' />
-        </Form.Field>
-        <Button>Add Item</Button>
-    </Form>
-)
+import { Component } from 'react'
+
+export default class extends Component {
+
+    addItem = (e) => {
+        e.preventDefault()
+        const itemToAdd = document.getElementById('itemName').value
+        const itemDescription = document.getElementById('itemDescription').value
+        this.props.addTodo(itemToAdd, itemDescription)
+    }
+
+    render () {
+        console.log(this.props.todoList)
+        return (
+            <Form onSubmit={this.addItem}>
+                <Form.Field>
+                    <input type='text' placeholder='Item to add' name="item" id='itemName' />
+                </Form.Field>
+                <Form.Field>
+                    <input type='text' placeholder='Item description' name="itemDescription" id='itemDescription' />
+                </Form.Field>
+                <Button type='submit'>Add Item</Button>
+            </Form>
+        )
+    }
+}
