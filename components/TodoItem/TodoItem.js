30917b2c5c597108222ebac636afa10e421f494f
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 15:44:26 -0600

    map through items

--- components/TodoItem/TodoItem.js
+++ components/TodoItem/TodoItem.js
@@ -1,5 +1,27 @@
-export default () => (
-    <div>
-        <h2>TodoItem</h2>
-    </div>
-)
+import { List } from 'semantic-ui-react'
+
+export default ({ todoList }) => {
+    return (
+        <div>
+            <h2>TodoItem</h2>
+            <List divided relaxed>
+                {
+                    todoList.map((item, id) => {
+                        return (
+                            <List.Item key={id}>
+                                <List.Content>
+                                    <List.Header>
+                                        {item.itemName}
+                                    </List.Header>
+                                    <List.Description>
+                                        {item.itemDescription}
+                                    </List.Description>
+                                </List.Content>
+                            </List.Item>
+                        )
+                    })
+                }
+            </List>
+        </div>
+    )
+}
