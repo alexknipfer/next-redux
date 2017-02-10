8ef808bd97ea557b8465b50243ccac0fa5faf86f
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 10:25:58 -0600

    container for layout

--- containers/LayoutContainer.js
+++ containers/LayoutContainer.js
@@ -1,10 +1,33 @@
 import { Component } from 'react'
 import Layout from '../components/Layout/Layout'
+import { actions } from '../redux/reducers/navigation'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 class LayoutContainer extends Component {
     render () {
         return (
-            <Layout />
+            <Layout>
+                {this.props.children}
+            </Layout>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        state
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        openDimmer: actions.openDimmer,
+        closeDimmer: actions.closeDimmer
+    }, dispatch)
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(LayoutContainer)
