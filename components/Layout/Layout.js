29dd9a414f71e44f75823f785a1aeb88cd8a57e4
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 10:59:55 -0600

    viewport

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -5,6 +5,7 @@ export default ({ children, open, openDimmer, closeDimmer }) => (
     <div>
         <Head>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />
         </Head>
         <Navigation open={open} openDimmer={openDimmer} closeDimmer={closeDimmer}/>
         {children}
