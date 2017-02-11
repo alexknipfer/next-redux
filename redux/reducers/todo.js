e25047a0222b7f554ef04ce62f52b3f5f1659cc9
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-10 21:03:06 -0600

    add remove action and reducer

--- redux/reducers/todo.js
+++ redux/reducers/todo.js
@@ -1,13 +1,26 @@
 export const constants = {
-    ADD_TODO: 'ADD_TODO'
+    ADD_TODO: 'ADD_TODO',
+    REMOVE_TODO: 'REMOVE_TODO'
 }
+
+let nextTodoId = 0
 
 export const actions = {
     addTodo(itemName, itemDescription) {
         return {
             type: constants.ADD_TODO,
-            itemToAdd: {itemName, itemDescription}
+            itemToAdd: { 
+                itemName: itemName, 
+                itemDescription: itemDescription,
+                id: nextTodoId++
+            }
         } 
+    },
+    removeTodo(id) {
+        return {
+            type: constants.REMOVE_TODO,
+            id: id
+        }
     }
 }
 
@@ -16,13 +29,19 @@ export const initialState = {
 }
 
 export default (state = initialState, action) => {
-    console.log(state)
     switch(action.type) {
         case constants.ADD_TODO:
             return {
                 ...state,
                 todos: [...state.todos, action.itemToAdd]
             }
+            
+        case constants.REMOVE_TODO:
+            return {
+                ...state,
+                todos: [...state.todos.filter((item) => item.id !== action.id)]
+            }
+
         default: 
             return state
     }
