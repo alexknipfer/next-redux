69ae63dbcffff87834bd9d094a3be67a1c261c84
Alex Knipfer <alexanderknipfer@gmail.com>
2017-02-11 20:34:52 -0600

    todo layout

--- components/ListView/ListView.js
+++ components/ListView/ListView.js
@@ -1,34 +1,37 @@
 import AddForm from '../AddForm/AddForm'
-import { Grid } from 'semantic-ui-react'
+import { Card, Icon, Grid } from 'semantic-ui-react'
 import TodoItem from '../TodoItem/TodoItem'
 
 export default ({ todoList, addTodo, removeTodo }) => {
     return (
         <div className='list-view'>
-            <Grid padded>
-                <Grid.Row>
-                    <Grid.Column>
-                        <h2>List View</h2>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <AddForm todoList={todoList} addTodo={addTodo} />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column>
-                        <TodoItem todoList={todoList} removeTodo={removeTodo} />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-            <style jsx>{`
-                .list-view {
-                    margin-top: 80px;
-                    display: flex;
-                    justify-content: center;
-                }
-            `}</style>
+            <Card>
+                <Grid padded>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Icon name='tasks' size='large' style={{display: 'inline', marginRight: '20px'}}/>
+                            <h2 style={{display: 'inline'}}>List View</h2>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <AddForm todoList={todoList} addTodo={addTodo} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <TodoItem todoList={todoList} removeTodo={removeTodo} />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+                </Card>
+                <style jsx>{`
+                    .list-view {
+                        margin-top: 80px;
+                        display: flex;
+                        justify-content: center;
+                    }
+                `}</style>
         </div>
     )
 }
