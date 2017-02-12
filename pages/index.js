235c523f4afdaa480ac9c94d7840b1150ddd7abc
Alex Knipfer <alexanderknipfer@gmail.com>
2017-02-11 20:26:31 -0600

    remove layout container

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import LayoutContainer from '../containers/LayoutContainer'
+import Layout from '../components/Layout/Layout'
 import { Provider } from 'react-redux'
 import TodoItemContainer from '../containers/TodoItemContainer'
 import store from '../redux/store'
 
 export default () => (
     <Provider store={store}>
-        <LayoutContainer>
+        <Layout>
             <TodoItemContainer />
-        </LayoutContainer>
+        </Layout>
     </Provider>
 )
--- redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import navigation from './navigation'
-import todo from './todo'
-
-export default combineReducers({
-    navigation,
-    todo
-})
