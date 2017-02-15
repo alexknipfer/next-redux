78bf3d9f2be93330e109e6e4681eaf44ed10afbf
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-14 20:35:45 -0600

    remove dimmer

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Navigation from '../Navigation/Navigation'
 
-export default ({ children, open, openDimmer, closeDimmer }) => (
+export default ({ children }) => (
     <div>
         <Head>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"></link>
             <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />
         </Head>
-        <Navigation open={open} openDimmer={openDimmer} closeDimmer={closeDimmer}/>
+        <Navigation />
         {children}
     </div>
 )
