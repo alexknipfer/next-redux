8795f665a1cf462264e5df98508bb52ec1f6a7ae
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-21 00:02:05 -0600

    redux devtools

--- redux/store.js
+++ redux/store.js
@@ -2,5 +2,8 @@ import { createStore } from 'redux'
 import rootReducer from './modules/rootReducer'
 
 export default createStore(
-    rootReducer
+    rootReducer,
+    typeof window !== 'undefined' && 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 )
